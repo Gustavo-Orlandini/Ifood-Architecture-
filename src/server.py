79365bb01fce76ed5d89fcf1358bf0c78ba408cf
*Gustavo <guga77sugu@hotmail.com>
2023-08-11 13:17:05 -0300

    Inform status code!

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('.')
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
 from src.schemas.schemas import Produto
@@ -11,12 +11,12 @@ criar_bd()
  
 app = FastAPI()
 
-@app.post('/produtos')
+@app.post('/produtos', status_code=status.HTTP_201_CREATED)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
-@app.get('/produtos')
+@app.get('/produtos', status_code=status.HTTP_200_OK)
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
